5da5964f12e16e2a09bab2ddb1573fc84ebfe5cb
wilsonft <wilsonft83@gmail.com>
2016-03-22 23:42:13 -0500

    chore(fonts):modificando path salida de fonts

--- frontend/config/gulpTasks/fonts.js
+++ frontend/config/gulpTasks/fonts.js
@@ -1,16 +1,16 @@
 module.exports = function Task(gulp,path,plugins,config){
   var dirList = [];
   var fonts = plugins.fs.readdirSync(path.frontend.fonts);
-    
+
   fonts.forEach(function(file){
     if(!/^_/.test(file)){
       dirList.push(file);
     }
-  }); 
-  console.log('dirList>>>',dirList); 
+  });
+  //console.log('dirList>>>',dirList);
   gulp.task('fonts',function(){
     return gulp.src(path.frontend.fonts+'/_template/fonts.css')
       .pipe(plugins.consolidate('lodash', { dirList: dirList }))
-      .pipe(gulp.dest(__dirname));
-  }); 
+      .pipe(gulp.dest(path.frontend.base+'/pages/all'));
+  });
 };
